d8225211a448cf662caefd32a30f756ca899d085
Kinjal Jain <kinjal@wolkensoftware.com>
2023-04-26 17:09:18 +0530

    Local Notification feature added

--- src/app/pages/pickup-call/pickup-call.page.ts
+++ src/app/pages/pickup-call/pickup-call.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { LocalNotifications } from '@capacitor/local-notifications';
+import { isPlatform } from '@ionic/angular';
 @Component({
   selector: 'app-pickup-call',
   templateUrl: './pickup-call.page.html',
@@ -10,9 +11,37 @@ export class PickupCallPage implements OnInit {
 
   constructor(private router: Router) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    await LocalNotifications.requestPermissions();
   }
-  OnCreatePickupCall(){
+  async OnCreatePickupCall(){
+    if(isPlatform('android') == true){
+      console.log('inside platform check');
+      LocalNotifications.createChannel({
+        id: '1',
+        name: 'local nitifications',
+        //sound: 'sound.wav'
+      });
+    }
+
+    const notifs = await LocalNotifications.schedule({
+      notifications :[
+        {
+          title: "Ionic app",
+          body: "checking local notification",
+          id: 1,
+          schedule: {at: new Date(Date.now() + 1000*5 )},
+          //sound: 'sound.wav',
+          //attachments: null,
+          //smallIcon: 'ic_stat_adb',
+          actionTypeId:"",
+          extra:{
+            data: 'Checking extra'
+          },
+        }
+      ]
+    });
+    console.log('scheduled notifications',notifs);
     this.router.navigate(['home']);
   }
 
