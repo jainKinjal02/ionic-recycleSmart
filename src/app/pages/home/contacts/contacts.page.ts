226fd57ce6bbfa0769db734dc7a766d610899b42
Kinjal Jain <kinjal@wolkensoftware.com>
2023-04-27 12:55:16 +0530

    access contacts native plugin added

--- src/app/pages/home/contacts/contacts.page.ts
+++ src/app/pages/home/contacts/contacts.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Contacts } from '@capacitor-community/contacts';
+import { Capacitor } from '@capacitor/core';
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.page.html',
@@ -10,10 +11,40 @@ export class ContactsPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.accessContacts();
   }
 
+  checkPlatformForWeb(){
+    if(Capacitor.getPlatform() == 'web') return true;
+    return false;
+  }
+  async accessContacts(){
+    if(!this.checkPlatformForWeb()){
+      try{
+         const permission =await Contacts.requestPermissions(); //only for android
+         console.log(permission);
+         if(!permission.contacts) return;
+         else if(permission.contacts == 'granted'){
+            const result = await Contacts.getContacts({
+              //add options
+              projection:{
+                name: true,
+                phones: true
+              }
+            });
+            this.contacts = result.contacts;
+            console.log(this.contacts);
+         }
+         
+      }catch(e){
+        console.log(e);
+      }
+    } else{
+      console.log('Contacts plugin is not for web');
+    }
+  }
   call(contact){
-
+    console.log(contact);
   }
 
 }
